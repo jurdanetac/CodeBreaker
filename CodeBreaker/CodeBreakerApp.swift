0fcda824a48c8a036c2eb5c699589ad4f6f09305
Juan Urdaneta <jurdanetac10@gmail.com>
2025-11-27 08:53:23 -0400

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Juan Urdaneta on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
